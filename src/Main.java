063ccaefd7c74d790213313d7925158872b09bcf
ViterEvgeniy <viter.evgeniy@gmail.com>
2024-02-23 19:19:04 +0300

    src

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 163;
+        if (price > 20) {
+            int oneBonus = 20;
+        int bonusMiles = (price/20);
+        System.out.println("Бонусные мили: " + bonusMiles);
+        }
+    }
+
+}
