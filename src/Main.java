731f98e1d5ec109e78ae1c458b5d0e8219a87360
ViterEvgeniy <viter.evgeniy@gmail.com>
2024-02-24 10:26:12 +0300

    chenged src

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         int price = 163;
-        if (price > 20) {
+        if (price >= 20) {
             int oneBonus = 20;
         int bonusMiles = (price/20);
         System.out.println("Бонусные мили: " + bonusMiles);
