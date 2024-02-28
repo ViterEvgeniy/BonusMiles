d8e863aab236b78fa33414bc44251a16f8d4de8a
Evgeniy Viter <viter.evgeniy@gmail.com>
2024-02-28 18:38:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int price = 163;
-        if (price >= 20) {
-           int oneBonus = 20;
-           int bonusMiles = (price / 20);
-           System.out.println("Бонусные мили: " + bonusMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
         }
     }
-
-}
